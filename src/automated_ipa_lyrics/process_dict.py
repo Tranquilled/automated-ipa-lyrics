355b392c436c58dd472465013b604bf0fefc91e6
Lavinia Lee <lavinia.lee@mail.mcgill.ca>
2025-09-30 05:04:21 -0400

    Scrape nonfunctional, connection problem?

--- src/automated_ipa_lyrics/process_dict.py
+++ src/automated_ipa_lyrics/process_dict.py
@@ -2,10 +2,26 @@ from os import sys
 from pathlib import Path
 import jsonlines
 import ipdb
+import wikipron
+
+
+def download_wiki_dict(lang_code: str) -> None:
+    """
+    TODO: add POS functionality to Wikipron
+    """
+    config = wikipron.Config(key="fra")  # French, with default options.
+    output_file = Path(f"../../raw_dicts/{lang_code}.tsv")
+    output_file.parent.mkdir(exist_ok=True, parents=True)
+    with open(output_file, "w") as f:
+        for word, pron in wikipron.scrape(config):
+            ipdb.set_trace()
+            f.write(f"{word}\t{pron}")
+
 
 def process_kaikki_dict(filename: Path) -> None:
     """
-    
+    Processes a manually-downloaded scrape of Wiktionary (1 language
+    at a time) from Kaikki.
     """
     output_file = f"{filename.stem}_trimmed.jsonl"
     phonemic_inventory = set()
@@ -38,6 +54,7 @@ def word_to_syllables(word_ipa: str):
 
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    filename = Path(filename)
-    process_kaikki_dict(filename)
+    # filename = sys.argv[1]
+    # filename = Path(filename)
+    # process_kaikki_dict(filename)
+    download_wiki_dict("fra")
