3ab462a6eb7b79fa4911ff8b546267ccaeee5b39
Lavinia Lee <lavinia.lee@mail.mcgill.ca>
2025-09-29 22:18:20 -0400

    Rebase

---
+++ src/automated_ipa_lyrics/main.py
@@ -0,0 +1,53 @@
+import re
+import requests
+import sys
+
+sources = {
+    "french": ["https://fr.wiktionary.org/w/index.php?search="],
+    "german": ["https://de.wiktionary.org/w/index.php?search="],
+}
+
+regexes = {
+    "https://fr.wiktionary.org/w/index.php?search=": re.compile(
+        r"\"Prononciation API\">(.+?)<\/span>"
+    )
+}
+
+
+def search(url, regex):
+    response = requests.get(url)
+    try:
+        found = regex.search(response.text)[1]
+        return found.replace("\\", "").replace("/", "")
+    except:
+        return False
+
+
+def lookup(word, language):
+    for url in sources[language]:
+        print(url + word)
+        regex = regexes[url]
+        print(regex)
+        scrape = search(url + word, regex)
+        if scrape and len(scrape) > 0:
+            return scrape
+    else:
+        return f"{word}"
+
+
+def main(filename, language):
+    with open(filename, "r") as f:
+        original = f.read()
+    original = (
+        original.replace("\n", " ")
+        .lower()
+    )
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+        language = sys.argv[2]
+
+    except:
+        print("Enter a word and its language")
