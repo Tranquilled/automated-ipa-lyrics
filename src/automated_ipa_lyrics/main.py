4cc7f8498137a7706601d7f2ac492b83b9735d45
Lavinia Lee <lavinia.lee@mail.mcgill.ca>
2025-10-14 01:11:02 -0400

    No more need to process my own dicts

--- src/automated_ipa_lyrics/main.py
+++ src/automated_ipa_lyrics/main.py
@@ -1,65 +1,25 @@
-import epitran
 import re
 import requests
 import sys
+from phonemizer import phonemize
+from phonemizer.separator import Separator
 
-sources = {
-    "french": ["https://fr.wiktionary.org/w/index.php?search="],
-    "german": ["https://de.wiktionary.org/w/index.php?search="],
-}
-
-regexes = {
-    "https://fr.wiktionary.org/w/index.php?search=": re.compile(
-        r"\"Prononciation API\">(.+?)<\/span>"
-    )
-}
-
-
-def search(url, regex):
-    response = requests.get(url)
-    try:
-        found = regex.search(response.text)[1]
-        return found.replace("\\", "").replace("/", "")
-    except:
-        return False
-
-
-def lookup(word, language):
-    for url in sources[language]:
-        print(url + word)
-        regex = regexes[url]
-        print(regex)
-        scrape = search(url + word, regex)
-        if scrape and len(scrape) > 0:
-            return scrape
-    else:
-        return f"{word}"
-
-
-def main(filename, language):
-    with open(filename, "r") as f:
-        original = f.read()
-    original = (
-        original.replace("\n", " ")
-        .lower()
-    )
-
-
-def epitran_process(source: str, lang_code: str) -> str:
-    epi = epitran.Epitran(lang_code)
-    output = epi.transliterate(source)
-    print(output)
-    return output
+def phonemizer_process(source: str , lang: str ) -> str:
+    phn = phonemize(
+        source,
+        language=lang,
+        backend='espeak', # pretty good!! IPA!!
+        separator=Separator(phone=None, word=' ', syllable='|'),
+        strip=True,
+        preserve_punctuation=True,
+        language_switch="remove-flags",
+        njobs=4)
+    return phn
 
 
 if __name__ == "__main__":
-    try:
-        filename = sys.argv[1]
-        language = sys.argv[2]
-
-    except:
-        print("Enter a string")
-        source = input()
-        print("Enter its language code")
-        language = input()
-        epitran_process(source, language)
+    print("Enter a string")
+    source = input() or "des oiseaux heureux couvent dans un couvent"
+    print("Enter its language code")
+    language = input() or "fr-fr"
+    print(phonemizer_process(source, language))
